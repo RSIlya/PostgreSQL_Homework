239d0d8a58e425b88208aa1f8d9c651f71e4240b
RSIlya <_ris_@mail.ru>
2022-02-12 22:30:01 +0500

    Homework database designe, connections.

---
+++ Scripts/ext_musik_db.sql
@@ -0,0 +1,38 @@
+create table if not exists Album(
+	id serial primary key,
+	title varchar(255)
+);
+create table if not exists Song(
+	id serial primary key,
+	album_id integer not null references Album(id),	
+	title varchar(255) not null,
+	duration integer
+);
+create table if not exists Collections(
+	id serial primary key,
+	name varchar(255) not null unique,
+	year integer check(Year > 1900)
+);
+create table if not exists CollectionsTrack(
+	collections_id integer not null references Collections(id),
+	song_id integer not null references Song(id),
+	constraint CT primary key (collections_id, song_id)
+);
+create table if not exists Artist(
+	id serial primary key,
+	name varchar(255) not null unique
+);
+create table if not exists ArtistAlbum(
+	artist_id integer not null references Artist(id),
+	album_id integer not null references Album(id),
+	constraint AA primary key (artist_id, album_id)
+);
+create table if not exists Genre(
+	id serial primary key,
+	name varchar(255) not null unique
+);
+create table if not exists GenreArtist(
+	genre_id integer not null references Genre(id),
+	artist_id integer not null references Artist(id),
+	constraint GA primary key (genre_id, artist_id)
+);
